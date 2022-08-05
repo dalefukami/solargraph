552b7db208baf06c855d6dfb0f339e77a518779f
Dale Fukami <dale.fukami@gmail.com>
2022-08-05 12:53:05 -0600

    Fine tune the variable replacement and don't provide extract var command unless whole words are selected

--- lib/solargraph/language_server/message/text_document/code_action.rb
+++ lib/solargraph/language_server/message/text_document/code_action.rb
@@ -16,7 +16,22 @@ module Solargraph
             line = original.split("\n")[params['range']['start']['line']]
             content = line[params['range']['start']['character']...params['range']['end']['character']]
 
+            return set_result([]) unless is_whole_word?(line, params['range']['start']['character'], params['range']['end']['character'])
+
+            # Ensure we only capture "whole words" and not leading trailing whitespace
+            trimmed_content = content.strip
+            start_chars_removed = 0
+            while content[start_chars_removed] != trimmed_content[0]
+              start_chars_removed += 1
+            end
+            end_chars_removed = content.length - trimmed_content.length - start_chars_removed
+
+            content = line[(params['range']['start']['character']+start_chars_removed)...(params['range']['end']['character']-end_chars_removed)]
+
+            # XXX: Clone range instead?
             selectionRange = params['range'] # XXX: trim the spaces around the selection?
+            selectionRange['start']['character'] += start_chars_removed
+            selectionRange['end']['character'] -= end_chars_removed
             selectionLine = params['range']['start']['line']
             # XXX: match indentation
             newVarRange = {
@@ -34,7 +49,7 @@ module Solargraph
                       "#{fileUri}": [
                         {
                           range: newVarRange,
-                          newText: "newvar = #{content}\n"
+                          newText: "newvar = #{trimmed_content}\n"
                         },
                         {
                           range: selectionRange,
@@ -47,11 +62,29 @@ module Solargraph
               ]
             )
           end
+
+          def is_whole_word?(line, start_char, end_char)
+            content = line[start_char...end_char]
+
+            trimmed_content = content.strip
+            start_chars_removed = 0
+            while content[start_chars_removed] != trimmed_content[0]
+              start_chars_removed += 1
+            end
+            end_chars_removed = content.length - trimmed_content.length - start_chars_removed
+
+            true_start = params['range']['start']['character'] + start_chars_removed
+            true_end = params['range']['end']['character'] - end_chars_removed
+            content = line[true_start...true_end]
+
+            # Hack... if we remove all non-variable-ish things from the surrounding characters and
+            # it matches the content we're trying to extract then we have "whole words"
+            # We should do real token parsing, etc.
+            extended_selection = line[(true_start-1)..true_end].gsub(/(^[^a-z'"_])|([^a-z'"_]$)/,'')
+            return content == extended_selection
+          end
         end
       end
     end
   end
 end
-
-
-# [DEBUG] {"jsonrpc"=>"2.0", "id"=>25, "method"=>"textDocument/codeAction", "params"=>{"textDocument"=>{"uri"=>"file:///data2/projects/app-shift-dealer/spec/services/fieldops/appointments/cancel_intent_service_spec.rb"}, "range"=>{"start"=>{"line"=>29, "character"=>13}, "end"=>{"line"=>29, "character"=>13}}, "context"=>{"diagnostics"=>[]}}}
--- spec/language_server/message/text_document/code_action_spec.rb
+++ spec/language_server/message/text_document/code_action_spec.rb
@@ -2,14 +2,14 @@ describe Solargraph::LanguageServer::Message::TextDocument::CodeAction do
   it 'extracts variable' do
     host = double(:Host, read_text: <<-CODE
 First line
-some code 'here'
+some code 'here' 
 Last Line
 CODE
     )
     request = {
       'params' => {
         'textDocument' => { 'uri' => 'test.rb' },
-        'range'=> { "start"=>{"line"=>1, "character"=>10}, "end"=>{"line"=>1, "character"=>15}}
+        'range'=> { "start"=>{"line"=>1, "character"=>10}, "end"=>{"line"=>1, "character"=>16}}
       }
     }
     message = Solargraph::LanguageServer::Message::TextDocument::CodeAction.new(host, request)
@@ -25,7 +25,97 @@ CODE
     expect(file_changes[1][:range]['start']['line']).to eq(1)
     expect(file_changes[1][:range]['start']['character']).to eq(10)
     expect(file_changes[1][:range]['end']['line']).to eq(1)
-    expect(file_changes[1][:range]['end']['character']).to eq(15)
+    expect(file_changes[1][:range]['end']['character']).to eq(16)
     expect(file_changes[1][:newText]).to eq("newvar")
+  end
+
+  it 'encompasses whole selection' do
+    host = double(:Host, read_text: <<-CODE
+if 'some string' && 'other string'
+CODE
+    )
+    request = {
+      'params' => {
+        'textDocument' => { 'uri' => 'test.rb' },
+        'range'=> { "start"=>{"line"=>0, "character"=>3}, "end"=>{"line"=>0, "character"=>16}}
+      }
+    }
+    message = Solargraph::LanguageServer::Message::TextDocument::CodeAction.new(host, request)
+    result = message.process
+    file_changes = result.first[:edit][:changes]["test.rb".to_sym]
+    file_changes = file_changes.map{|f| f.transform_keys(&:to_sym)}
+    expect(file_changes.first[:range][:start][:line]).to eq(0)
+    expect(file_changes.first[:range][:end][:line]).to eq(0)
+    expect(file_changes.first[:newText]).to eq("newvar = 'some string'\n")
+
+    range = file_changes[1][:range]
+    expect(range['start']['line']).to eq(0)
+    expect(range['start']['character']).to eq(3)
+    expect(range['end']['line']).to eq(0)
+    expect(range['end']['character']).to eq(16)
+    expect(file_changes[1][:newText]).to eq("newvar")
+  end
+
+  it 'trims selection' do
+    host = double(:Host, read_text: <<-CODE
+if 'some string' && 'other string'
+CODE
+    )
+    request = {
+      'params' => {
+        'textDocument' => { 'uri' => 'test.rb' },
+        'range'=> { "start"=>{"line"=>0, "character"=>2}, "end"=>{"line"=>0, "character"=>17}}
+      }
+    }
+    message = Solargraph::LanguageServer::Message::TextDocument::CodeAction.new(host, request)
+    result = message.process
+    file_changes = result.first[:edit][:changes]["test.rb".to_sym]
+    file_changes = file_changes.map{|f| f.transform_keys(&:to_sym)}
+    expect(file_changes.first[:range][:start][:line]).to eq(0)
+    expect(file_changes.first[:range][:end][:line]).to eq(0)
+    expect(file_changes.first[:newText]).to eq("newvar = 'some string'\n")
+
+    range = file_changes[1][:range]
+    expect(range['start']['line']).to eq(0)
+    expect(range['start']['character']).to eq(3)
+    expect(range['end']['line']).to eq(0)
+    expect(range['end']['character']).to eq(16)
+    expect(file_changes[1][:newText]).to eq("newvar")
+  end
+
+  it 'does not return action if whole word not selected' do
+    host = double(:Host, read_text: <<-CODE
+if 'some string' && 'other string'
+CODE
+    )
+    request = {
+      'params' => {
+        'textDocument' => { 'uri' => 'test.rb' },
+        'range'=> { "start"=>{"line"=>0, "character"=>5}, "end"=>{"line"=>0, "character"=>6}}
+      }
+    }
+    message = Solargraph::LanguageServer::Message::TextDocument::CodeAction.new(host, request)
+
+    result = message.process
+
+    expect(result).to eq([])
+  end
+
+  it 'considers non-variable things as word surrounders' do
+    host = double(:Host, read_text: <<-CODE
+if ('some string') && 'other string'
+CODE
+    )
+    request = {
+      'params' => {
+        'textDocument' => { 'uri' => 'test.rb' },
+        'range'=> { "start"=>{"line"=>0, "character"=>4}, "end"=>{"line"=>0, "character"=>17}}
+      }
+    }
+    message = Solargraph::LanguageServer::Message::TextDocument::CodeAction.new(host, request)
+
+    result = message.process
+
+    expect(result.length).to eq(1)
   end
 end
